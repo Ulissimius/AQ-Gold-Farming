c7650f35dd570d1763273f1605f71c4dfa3d2ff7
Mathew Pavao <46928427+Ulissimius@users.noreply.github.com>
2024-10-14 11:21:18 -0400

    Fixed some spelling mistakes. Adjusted confidence value in Gogg and PC HP checks to be higher. Increased speed of attack checks to shorten wait when battle is over.

--- source.py
+++ source.py
@@ -5,7 +5,7 @@ import threading
 
 # Things to do:
 # Set up picture reading
-# Make a one time initialization of focusing visit neihbor text box
+# Make a one time initialization of focusing visit neighbor text box
 # Make a one time initialization of turning on Shield Buff + Overdrive toggle
 # Make a conditional to handle dying
 
@@ -39,7 +39,7 @@ def farming_loop():
         pyautogui.write('45387657', interval=0.01)
         find_and_press(filePathMain + 'travel_to_neighbor_button.png')
 
-        # Battle Screen: (IR) buff and overdive -> click attack -> click attack/click done -> click done
+        # Battle Screen: (IR) buff and overdrive -> click attack -> click attack/click done -> click done
         print('Battle Screen')
         while is_gog_alive(): # There will need to be a catch here to handle player death
             if not is_pc_alive():
@@ -55,7 +55,7 @@ def farming_loop():
                     find_and_press(filePathInitial + 'OT_shield.png')
                 if shieldBuff and overdrive:
                     initialRun = False
-            find_and_press(filePathMain + 'attack_button.png')
+            find_and_press(filePathMain + 'attack_button.png', True)
         if not is_pc_alive():
             find_and_press(filePathDeath + 'next.png')
             time.sleep(2)
@@ -83,8 +83,14 @@ def stop_farming():
     running = False
     print("Stopping farming loop...")
 
-def find_and_press(path):
+def find_and_press(path, countOverride=False):
     global counter, shieldBuff, overdrive
+
+    # Used to shorten attack loop checks to speed up the result screen check.
+    if countOverride:
+        counter = 8
+        print('Counter Shortened!')
+    
     try:
         # Locate the image on the screen and return its coordinates
         button_location = pyautogui.locateOnScreen(path, confidence=0.9)
@@ -95,11 +101,13 @@ def find_and_press(path):
             find_and_press(path)
         else:
             counter = 0
-            return print('Image not found!')
+            return print('Image not found! Skipping action...')
     else:
         # If found, click the center of the located area
         if button_location:
             pyautogui.click(pyautogui.center(button_location))
+        
+        # These checks mark player buffs as active for the run.
         if path == 'images/2 - initializing images/OT_overdrive_onslaught_back.png':
             overdrive = True
         if path == 'images/2 - initializing images/OT_shield.png':
@@ -107,19 +115,21 @@ def find_and_press(path):
 
         counter = 0
 
+# Checks to see if the monster is dead.
 def is_gog_alive():
     try:
-        pyautogui.locateOnScreen('images/1 - main loop images/hp_0.png', confidence=0.95)
+        pyautogui.locateOnScreen('images/1 - main loop images/hp_0.png', confidence=0.99)
     except pyautogui.ImageNotFoundException:
-        print('The Shadow Gog lives!')
+        print('The Shadow Gogg lives!')
         return True
     else:
-        print('The Shadow Gog is dead!')
+        print('The Shadow Gogg is dead!')
         return False
-    
+
+# Checks to see if the PC has died.
 def is_pc_alive():
         try:
-            pyautogui.locateOnScreen('images/3 - death event images/pc_dead.png', confidence=0.95)
+            pyautogui.locateOnScreen('images/3 - death event images/pc_dead.png', confidence=0.99)
         except pyautogui.ImageNotFoundException:
             print('PC is alive.')
             return True
