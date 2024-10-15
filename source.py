ceadbdb83cdb3ae8e784d9ae3436adfa62059178
Mathew Pavao <46928427+Ulissimius@users.noreply.github.com>
2024-10-15 11:23:27 -0400

    Updated 'next.png.' Added a new hotkey, F7, to start without buffing (mostly for testing). Updated PC death check to account for long Gogg attacks. Changed count override parameter to an int instead of a bool to allow for flexibility.

--- source.py
+++ source.py
@@ -3,12 +3,6 @@ import keyboard
 import time
 import threading
 
-# Things to do:
-# Set up picture reading
-# Make a one time initialization of focusing visit neighbor text box
-# Make a one time initialization of turning on Shield Buff + Overdrive toggle
-# Make a conditional to handle dying
-
 running = False
 initialRun = True
 counter = 0 # recursive tracking
@@ -30,18 +24,17 @@ def farming_loop():
         print("Home Screen")
         find_and_press(filePathMain + 'fountain.png')
         find_and_press(filePathMain + 'to_house_button.png')
-        # time.sleep(1) # loading screen pause
 
         # My House screen: click visit neighbor -> (IR) click again to focus -> Ctrl + V -> click specific neighbor
         print('My House')
         find_and_press(filePathMain + 'visit_neighbor_button.png')
-        pyautogui.click()
-        pyautogui.write('45387657', interval=0.01)
+        pyautogui.click() # Click once to focus the text box on the first pass
+        pyautogui.write('45387657', interval=0.01) # This is the neighbor code
         find_and_press(filePathMain + 'travel_to_neighbor_button.png')
 
         # Battle Screen: (IR) buff and overdrive -> click attack -> click attack/click done -> click done
         print('Battle Screen')
-        while is_gog_alive(): # There will need to be a catch here to handle player death
+        while is_gogg_alive():
             if not is_pc_alive():
                 break
             while initialRun: # Activates important buffs/toggles for the loop
@@ -55,7 +48,7 @@ def farming_loop():
                     find_and_press(filePathInitial + 'OT_shield.png')
                 if shieldBuff and overdrive:
                     initialRun = False
-            find_and_press(filePathMain + 'attack_button.png', True)
+            find_and_press(filePathMain + 'attack_button.png', 8)
         if not is_pc_alive():
             find_and_press(filePathDeath + 'next.png')
             time.sleep(2)
@@ -72,8 +65,16 @@ def farming_loop():
         find_and_press(filePathMain + 'back_to_town_button.png')
 
 def start_farming():
-    global running
+    global running, initialRun
     if not running:  # To prevent starting multiple threads
+        running = True
+        thread = threading.Thread(target=farming_loop)
+        thread.start()
+
+def resume_farming():
+    global running, initialRun
+    if not running:  # To prevent starting multiple threads
+        initialRun = False
         running = True
         thread = threading.Thread(target=farming_loop)
         thread.start()
@@ -83,13 +84,12 @@ def stop_farming():
     running = False
     print("Stopping farming loop...")
 
-def find_and_press(path, countOverride=False):
+def find_and_press(path, countOverride=0):
     global counter, shieldBuff, overdrive
 
-    # Used to shorten attack loop checks to speed up the result screen check.
-    if countOverride:
-        counter = 8
-        print('Counter Shortened!')
+    # Used to lengthen or shorten the counter to slow down or speed up the checks where needed
+    if countOverride != 0:
+        counter = countOverride
     
     try:
         # Locate the image on the screen and return its coordinates
@@ -116,7 +116,7 @@ def find_and_press(path, countOverride=False):
         counter = 0
 
 # Checks to see if the monster is dead.
-def is_gog_alive():
+def is_gogg_alive():
     try:
         pyautogui.locateOnScreen('images/1 - main loop images/hp_0.png', confidence=0.99)
     except pyautogui.ImageNotFoundException:
@@ -128,19 +128,32 @@ def is_gog_alive():
 
 # Checks to see if the PC has died.
 def is_pc_alive():
+
+    def is_gogg_done(): # Function to see if The Shadow Gogg is still attacking while PC is dead
         try:
-            pyautogui.locateOnScreen('images/3 - death event images/pc_dead.png', confidence=0.99)
+            pyautogui.locateOnScreen('images/3 - death event images/next.png', confidence=0.95)
         except pyautogui.ImageNotFoundException:
-            print('PC is alive.')
             return True
         else:
-            print('Oh no, you\'ve died!')
             return False
 
+    try:
+        pyautogui.locateOnScreen('images/3 - death event images/pc_dead.png', confidence=0.99)
+    except pyautogui.ImageNotFoundException:
+        print('PC is alive.')
+        return True
+    else:
+        while is_gogg_done():
+            time.sleep(1)
+            print('The Shadow Gogg is still brutalizing you, please hold...')
+        print('The Shadow Gogg has allowed you to perish.\nOh no, you\'ve died!')
+        return False
+
 # Set hotkeys for starting and stopping
-keyboard.add_hotkey('F8', start_farming)  # Press F8 to start
-keyboard.add_hotkey('F9', stop_farming)   # Press F9 to stop
+keyboard.add_hotkey('F7', resume_farming)  # Press F7 to start without buffs
+keyboard.add_hotkey('F8', start_farming)   # Press F8 to start
+keyboard.add_hotkey('F9', stop_farming)    # Press F9 to stop
 
 # Keeps the program running, waiting for hotkey presses
-print("Press F8 to start farming, F9 to stop.")
+print("Press F8 to start farming, F9 to stop, F7 to start without buffing, and esc to exit the script.")
 keyboard.wait('esc')  # Press Esc to exit the script completely
